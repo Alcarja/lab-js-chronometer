93ec40a7c723599476e170793da449e28ce5a366
Alcaraz <jaime.mediano@gmail.com>
2023-06-20 16:21:04 +0200

    I think my brain doesn´t work. I need professional help to understand code. Might be dumber than the average.

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,33 +1,52 @@
 class Chronometer {
   constructor() {
     // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
   }
 
   start(printTimeCallback) {
-    // ... your code goes here
+    this.intervalId = setInterval (() => {
+      this.currentTime += 1;
+      if (printTimeCallback){
+        printTimeCallback();
+      } else {
+        return 0;
+      }
+    }, 1000)
   }
 
   getMinutes() {
-    // ... your code goes here
+    let entireMinutes = Math.floor(this.currentTime / 60);
+    return entireMinutes;
   }
 
   getSeconds() {
-    // ... your code goes here
+    let totalSeconds = this.currentTime % 60
+    return totalSeconds; // Returns the reminder after dividing by 60. After getting the minutes (dividing by 60) it returns the seconds left. The remainder (%) operator returns the remainder left over when one operand is divided by a second operand.
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
-  }
+    let valueString = String(value);
+    if(valueString.length <= 1) {
+      valueString = '0' + valueString;
+      return valueString;
+    } else {return valueString};
+  } //We convert everything into a string with 'String(value)'. If the string is smaller than 2 digits we add a 0 in front. If it is 2 digits we simply return it.
+
+  // return ('0' + value).slice(-2); 
 
   stop() {
-    // ... your code goes here
+    clearInterval(this.intervalId);
   }
 
   reset() {
-    // ... your code goes here
+    this.currentTime = 0;
   }
 
   split() {
-    // ... your code goes here
+    let stringEntireMinutes = this.computeTwoDigitNumber(this.getMinutes());
+    let stringTotalSeconds = this.computeTwoDigitNumber(this.getSeconds());
+    return `${stringEntireMinutes}:${stringTotalSeconds}`;
   }
 }
